17a56fbfcb64c1d453c1cb476b2b87f0ab7e3e10
Kaique Araujo Pinholato Ribeiro <kaiquepinho2010@hotmail.com>
2023-09-12 21:31:04 -0300

    fix: add fronted spa

---
+++ src/main/resources/script.js
@@ -0,0 +1,21 @@
+var inputFiles = [];
+
+function newInput(input) {
+    var filesStr = "";
+
+    for (let i = 0; i < input.files.length; i++) {
+        inputFiles.push(input.files[i]);
+        filesStr += "<li>" + input.files[i].name + "<button onclick='removeLi(this)'>Remover</button>" + "</li>";
+    }
+
+    document.getElementById("file-input").value = "";
+
+    document.getElementById("dp-files").innerHTML += filesStr;
+}
+
+function removeLi(e) {
+    inputFiles = inputFiles.filter(function (file) {
+        return file.name !== e.parentNode.innerHTML.split("<button")[0];
+    })
+    e.parentNode.parentNode.removeChild(e.parentNode);
+}
